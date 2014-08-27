8c7e598a47ae702af999df918543cb6ce8750053
Nik Ormseth <nik@ormseth.net>
2014-08-27 13:12:20 -0700

    fixed reference to obsolete attribute

--- recipes/dbserver.rb
+++ recipes/dbserver.rb
@@ -6,16 +6,17 @@ connection_info = {:host => node['rails_infrastructure']['dbhost'],
                   :port => node['postgresql']['config']['port'],
                   :username => 'postgres'}
 
-dbname = "#{node['rails_infrastructure']['app_db_user']['name']}_#{node['rails_infrastructure']['environment']}"
+dbname = "#{node['rails_infrastructure']['app']['name']}_#{node.chef_environment}"
 
 postgresql_database dbname do
   connection connection_info
   action :create
 end
 
-postgresql_database_user node['rails_infrastructure']['app_db_user']['name'] do
+db_creds = encrypted_data_bag_item_for_environment('creds', 'db_creds')
+postgresql_database_user db_creds['microblog_ruby_dbuser'] do
   connection connection_info
-  password node['rails_infrastructure']['app_db_user']['password']
+  password db_creds['microblog_ruby_dbpass']
   database_name dbname
   action [ :create, :grant ]
 end
