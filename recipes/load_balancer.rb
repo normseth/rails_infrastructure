e3590f44cbc8e4a050d3275428caa2321743fca6
Nik Ormseth <nik@ormseth.net>
2014-09-18 10:25:48 -0700

    fix ip address to work both vagrant and ec2

--- recipes/load_balancer.rb
+++ recipes/load_balancer.rb
@@ -2,7 +2,7 @@ include_recipe 'rails_infrastructure::common'
 include_recipe 'nginx'
 
 # TODO: Fix the following.  Pretty fragile, although it works correctly with my Vagrant and EC2 setups.
-
+# Note that kitchen-ec2 doesn't set ohai hint, so fails to detect and chef-sugar "ec2?" method fails.
 if node['current_user'] == 'vagrant' then
   preferred_local_ip = node['network']['interfaces']['eth1']['addresses'].keys[1]
 else
