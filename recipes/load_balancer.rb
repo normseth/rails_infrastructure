4ada71b6d9df77b7a257e91389884cc27e98cdbc
Nik Ormseth <nik@ormseth.net>
2014-09-18 08:28:25 -0700

    fix ip address to work both vagrant and ec2

--- recipes/load_balancer.rb
+++ recipes/load_balancer.rb
@@ -1,8 +1,13 @@
 include_recipe 'rails_infrastructure::common'
 include_recipe 'nginx'
 
-# TODO: Fix the following.  It is very fragile.
-preferred_local_ip = node['network']['interfaces']['eth1']['addresses'].keys[1]
+# TODO: Fix the following.  Pretty fragile, although it works correctly with my Vagrant and EC2 setups.
+
+if node['vagrant'] then
+  preferred_local_ip = node['network']['interfaces']['eth1']['addresses'].keys[1]
+else
+  preferred_local_ip = node['ipaddress']
+end
 
 # TODO: Fix the template source -- it assumes we're just passing a single IP address
 template "#{node['nginx']['dir']}/conf.d/app.conf" do
